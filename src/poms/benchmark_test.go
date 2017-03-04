0101c64068c889f785a1676cfa80dda0e0c2da9d
zhanat <iskakov_zhanat@mail.ru>
2017-03-04 12:06:55 +0600

    wep app benchmark test

---
+++ src/poms/benchmark_test.go
@@ -0,0 +1,35 @@
+package poms
+
+import (
+	"net/http"
+	"os"
+	"poms/ctrl"
+	"testing"
+	"fmt"
+)
+
+// запускается перед каждым тестом
+func TestMain(m *testing.M) {
+	ctrl.Setup()
+
+	go http.ListenAndServe(":3000", new(GZipServer))
+
+	// запускает все тесты
+	fmt.Println("before run all test")
+	m.Run()
+	fmt.Println("after run all test")
+
+	os.Exit(0)
+}
+
+func BenchmarkGetPurchaseOrder(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		http.Get("http://localhost:3000/api/purchaseOrders/1")
+	}
+}
+
+func BenchmarkGetCurrencies(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		http.Get("http://localhost:3000/api/currencies")
+	}
+}
