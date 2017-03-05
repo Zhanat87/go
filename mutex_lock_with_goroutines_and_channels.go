0127e42e782cf48fea72747118a3d24395de46f9
zhanat <iskakov_zhanat@mail.ru>
2017-03-05 18:05:30 +0600

    mutex_lock_with_goroutines_and_channels

--- mutex_lock_with_goroutines_and_channels.go
+++ mutex_lock_with_goroutines_and_channels.go
@@ -3,18 +3,38 @@ package main
 import (
 	"runtime"
 	"fmt"
-	"sync"
+	"os"
+	"time"
 )
 
 func main() {
 	runtime.GOMAXPROCS(4)
-	mutex := new(sync.Mutex)
+	logFile := "./log.txt"
+	f, _ := os.Create(logFile)
+	f.Close()
+	logCh := make(chan string, 50)
+	go func() {
+		for {
+			msg, ok := <- logCh
+			if ok {
+				f, _ := os.OpenFile(logFile, os.O_APPEND, os.ModeAppend)
+				logTime := time.Now().Format(time.RFC3339)
+				f.WriteString(logTime + " - " + msg)
+				f.Close()
+			} else {
+				break
+			}
+		}
+	}()
+	mutex := make(chan bool, 1)
 	for i := 0; i < 10; i++ {
 		for j := 0; j < 10; j++ {
-			mutex.Lock()
+			mutex <- true
 			go func() {
-				mutex.Unlock()
-				fmt.Printf("%d + %d = %d\r\n", i, j, i + j)
+				msg := fmt.Sprintf("%d + %d = %d\r\n", i, j, i + j)
+				logCh <- msg
+				fmt.Print(msg)
+				<- mutex
 			}()
 		}
 	}
