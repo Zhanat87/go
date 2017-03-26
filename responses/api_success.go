450bf1d4f94f9e052f543d3988e12c756150f1a5
zhanat <iskakov_zhanat@mail.ru>
2017-03-26 11:15:19 +0600

    api_responses

--- responses/api_success.go
+++ responses/api_success.go
@@ -1,11 +1,20 @@
 package responses
 
+import "encoding/json"
+
 // APISuccess represents an error that can be sent in an error response.
 type APISuccess struct {
 	// Status represents the HTTP status code
-	Status int `json:"200"`
+	Status int `json:"status"`
 	// Message is the error message that may be displayed to end users
-	Message string `json:"ok"`
+	Message string `json:"message"`
 	// Data specifies the additional response information
 	Data struct{} `json:"data,omitempty"`
+}
+
+func (s *APISuccess) MarshalJSON() ([]byte, error) {
+	return json.Marshal(&APISuccess{
+		Status:       200,
+		Message:     "ok",
+	})
 }
