e1d162a6cbf49572d06902b326afff4b8aa14634
zhanat <iskakov_zhanat@mail.ru>
2017-03-26 17:33:42 +0600

    albums

--- models/album.go
+++ models/album.go
@@ -1,7 +1,17 @@
 package models
 
+import "github.com/go-ozzo/ozzo-validation"
+
 type Album struct {
 	Id       int    `json:"id" db:"id"`
-	Title    string  `json:"title" db:"title"`
-	ArtistId uint    `json:"artistId" db:"artist_id"`
+	Title    string `json:"title" db:"title"`
+	ArtistId uint   `json:"artistId" db:"artist_id"`
+}
+
+// Validate validates the Artist fields.
+func (m Album) Validate() error {
+	return validation.ValidateStruct(&m,
+		validation.Field(&m.Title, validation.Required, validation.Length(0, 120)),
+		validation.Field(&m.ArtistId, validation.Required, validation.ToInt(&m.ArtistId)),
+	)
 }
