0822a6e292b5bb88e5e869e5f25619d9f1a180b2
zhanat <iskakov_zhanat@mail.ru>
2017-03-26 11:30:12 +0600

    api_responses

--- responses/api_success.go
+++ responses/api_success.go
@@ -1,6 +1,6 @@
 package responses
 
-import "encoding/json"
+//import "encoding/json"
 
 // APISuccess represents an error that can be sent in an error response.
 type APISuccess struct {
@@ -13,9 +13,9 @@ type APISuccess struct {
 }
 
 // http://choly.ca/post/go-json-marshalling/
-func (s *APISuccess) MarshalJSON() ([]byte, error) {
-	return json.Marshal(&APISuccess{
-		Status:  200,
-		Message: "ok",
-	})
-}
+//func (s *APISuccess) MarshalJSON() ([]byte, error) {
+//	return json.Marshal(&APISuccess{
+//		Status:  200,
+//		Message: "ok",
+//	})
+//}
