1ce499f00338d1d6c98d9ffeaffe623d66d3c7e5
zhanat <iskakov_zhanat@mail.ru>
2017-03-26 18:08:28 +0600

    albums

--- models/album.go
+++ models/album.go
@@ -1,7 +1,17 @@
 package models
 
-import "github.com/go-ozzo/ozzo-validation"
+import (
+	"github.com/go-ozzo/ozzo-validation"
+	//"time"
+	//"github.com/Zhanat87/go/db"
+)
 
+/*
+https://github.com/go-ozzo/ozzo-dbx/issues/23
+dbx is not orm and can do relational queries, etc with relations
+need select by separate queries,
+find parent model and then related models by foreign id(s)
+ */
 type Album struct {
 	Id       int    `json:"id" db:"id"`
 	Title    string `json:"title" db:"title"`
@@ -15,3 +25,21 @@ func (m Album) Validate() error {
 		validation.Field(&m.ArtistId, validation.Required),
 	)
 }
+
+//func (u *Album) BeforeInsert(){
+//	u.CreatedAt = time.Now()
+//	u.UpdatedAt= u.CreatedAt
+//}
+//
+//func (u *User) BeforeUpdate(){
+//	u.UpdatedAt = time.Now()
+//}
+//
+//func (u *User) Create() error{
+//	u.BeforeInsert()
+//	err:= db.Model(u).Insert()
+//	if err != nil{
+//		println("Exec err:", err.Error())
+//	}
+//	return err
+//}
