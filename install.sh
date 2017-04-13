fc9c6b0b17d3d4f0d52adc5588312fdefcd8cfdf
zhanat <iskakov_zhanat@mail.ru>
2017-04-13 11:12:01 +0600

    change in local

--- install.sh
+++ install.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
 rm -rf src/google.golang.org/appengine/
-git add . && git commit -m 'install' && git pull origin master
+git add .
+git commit -m 'install'
+git pull origin master
 # remove container
 docker rm $(docker ps -a -q --filter ancestor=zhanat87/golang) -f
 # remove image
 docker rmi $(docker images --filter=reference='zhanat87/golang') -f
 # rm postgresql if needed
-docker rm postgresql
-docker rmi postgres
+#docker rm postgresql
+#docker rmi postgres
 # start docker-compose
 cd docker && docker-compose up -d
 docker exec -it golang /go/bin/migrate -url postgres://postgres:postgres@postgresql:5432/go_restful?sslmode=disable -path /go/migrations up
