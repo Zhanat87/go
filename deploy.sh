d71ed87dc2f635252420dd583002d595795293a5
zhanat <iskakov_zhanat@mail.ru>
2017-04-19 15:18:45 +0600

    deploy

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,8 @@
 git add . && git commit -m 'deploy' && git push origin master
 # stop & remove all docker containers
 docker stop $(docker ps -a -q)
-docker rm $(docker ps -a -q)
+# not need remove, because db data was deleted in postgres
+#docker rm $(docker ps -a -q)
 # delete all images
 #docker rmi $(docker images -q)
 # remove container
@@ -16,9 +17,9 @@ rm bin/go
 go get -u github.com/Zhanat87/go
 cd src/github.com/Zhanat87/go/ && go install && cd ../../../../
 # create new docker image, push to docker hub and pull
-#docker build -t zhanat87/golang .
-#docker push zhanat87/golang
-#docker pull zhanat87/golang
+docker build -t zhanat87/golang .
+docker push zhanat87/golang
+docker pull zhanat87/golang
 # list of all docker images on host machine
 docker images
 
