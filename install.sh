869c3a8ad9d3bf7d307079ae765eab1842fc4484
zhanat <iskakov_zhanat@mail.ru>
2017-04-29 09:29:01 +0600

    deploy

--- install.sh
+++ install.sh
@@ -10,22 +10,22 @@ docker rm $(docker ps -a -q)
 # remove container
 #docker rm $(docker ps -a -q --filter ancestor=zhanat87/golang) -f
 ## remove image and pull new
-#docker rmi $(docker images --filter=reference='zhanat87/golang') -f
+docker rmi $(docker images --filter=reference='zhanat87/golang') -f
 # delete all images
-docker rmi $(docker images -q)
+#docker rmi $(docker images -q)
 # pull docker containers
 docker pull zhanat87/golang
-docker pull postgres
+#docker pull postgres
 # rm postgresql if needed
 #docker rm postgresql
 #docker rmi postgres
 # start docker-compose
 # sudo service postgresql stop
-#cd docker && docker-compose up -d
-#docker exec -it zhanat87/golang /go/migrate -url postgres://postgres:postgres@postgresql:5432/go_restful?sslmode=disable -path /go/migrations up
+cd docker && docker-compose up -d
+docker exec -it zhanat87/golang /go/migrate -url postgres://postgres:postgres@postgresql:5432/go_restful?sslmode=disable -path /go/migrations up
 # list of all docker images on host machine
 # build client
-cd client && npm run prebuild:prod && npm run build:prod
+#cd ../client && npm run prebuild:prod && npm run build:prod
 docker images
 # after docker containers start
 #docker exec -it restful /bin/bash
