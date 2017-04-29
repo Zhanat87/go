1e58e74e2ac8a7dcb87680bbdabd5ab1ac1de7b4
zhanat <iskakov_zhanat@mail.ru>
2017-04-29 16:49:06 +0600

    last changes

--- server.go
+++ server.go
@@ -76,7 +76,7 @@ func buildRouter(logger *logrus.Logger, db *dbx.DB, dsn string) *routing.Router 
 		} else {
 			variables += "success connected\r\n"
 		}
-		return c.Write(variables + "\r\n" + dsn + "\r\ndeploy\r\naot compilation\r\nwebhook\r\n")
+		return c.Write(variables + "\r\n" + dsn + "\r\ndeploy\r\naot compilation\r\nwebhook2\r\n")
 	})
 
 	router.Use(
