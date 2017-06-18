d7ef6b8bd4d9c832e4de07f6ce674371a1a42536
zhanat <iskakov_zhanat@mail.ru>
2017-06-18 11:00:58 +0600

    deploy

--- bash/test.go
+++ bash/test.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	for i := 0; i < 10; i++ {
+	for i := 0; i < 100; i++ {
 		f, err := os.OpenFile("test.log", os.O_APPEND|os.O_CREATE|os.O_RDWR, 0666)
 		if err != nil {
 			panic(err)
@@ -16,6 +16,6 @@ func main() {
 		defer f.Close()
 		log.SetOutput(f)
 		log.Print("test " + strconv.Itoa(i))
-		time.Sleep(time.Minute)
+		time.Sleep(time.Second * 10)
 	}
 }
