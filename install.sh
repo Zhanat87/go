6fe1eec05123e887e83bda44c459e5a0f2dff392
zhanat <iskakov_zhanat@mail.ru>
2017-06-18 08:10:58 +0600

    deploy

--- install.sh
+++ install.sh
@@ -53,7 +53,7 @@ MONGODB_DSN=mongo:27017 ../cli/mcdonaldsMenu/mcdonaldsMenu
 
 # run monitoring script for check state
 kill -9 "echo "$(cat pid.txt)""
-GRPC_SERVER=192.168.0.3:50051 DOMAIN_NAME=zhanat.site API_BASE_URL=http://zhanat.site:8080/ ../cli/monitoring/monitoring &
+../cli/monitoring/monitoring &
 echo $! | tee pid.txt
 
 # list of all docker images on host machine
